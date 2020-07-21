a7b02fdeb41d4b78472efac9e1734d401979dd52
eureka84 <angelosciarra@ymail.com>
2020-07-21 17:51:04 +0200

    update kotlin and arrow version

--- src/main/kotlin/it/eureka/katas/birthdaygreeting/Helper.kt
+++ src/main/kotlin/it/eureka/katas/birthdaygreeting/Helper.kt
@@ -4,20 +4,19 @@ import arrow.core.extensions.list.traverse.sequence
 import arrow.core.fix
 import arrow.fx.ForIO
 import arrow.fx.IO
-import arrow.fx.extensions.io.applicative.applicative
 import arrow.fx.extensions.io.functor.functor
 import arrow.fx.extensions.io.monad.monad
 import arrow.mtl.EitherT
 import arrow.mtl.extensions.eithert.applicative.applicative
 import arrow.mtl.fix
 
-fun <E, A> List<EitherT<ForIO, E, A>>.sequence(): EitherT<ForIO, E, List<A>> {
-    val applicative = EitherT.applicative<ForIO, E>(IO.applicative())
+fun <E, A> List<EitherT<E, ForIO, A>>.sequence(): EitherT<E, ForIO, List<A>> {
+    val applicative = EitherT.applicative<E, ForIO>(IO.monad())
     return this.sequence(applicative).fix().map(IO.functor()) { it.fix() }
 }
 
-fun <E, A, B> EitherT<ForIO, E, A>.map(f: (A) -> B): EitherT<ForIO, E, B> =
+fun <E, A, B> EitherT<E, ForIO, A>.map(f: (A) -> B): EitherT<E, ForIO, B> =
     this.map(IO.functor(), f)
 
-fun <E, A, B> EitherT<ForIO, E, A>.flatMap(f: (A) -> EitherT<ForIO, E, B>): EitherT<ForIO, E, B> =
+fun <E, A, B> EitherT<E, ForIO, A>.flatMap(f: (A) -> EitherT<E, ForIO, B>): EitherT<E, ForIO, B> =
     this.flatMap(IO.monad(), f)
